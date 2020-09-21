272411a11a0848b08e522cadbf59d99afc1c626e
angten <2258965667@qq.com>
2020-09-21 20:19:20 +0800

    参数传递与 前置或后置守卫或钩子

---
+++ src  9-21-2/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-link to="/home" tag="button" replace>主页</router-link>
+    <router-link to="/about" tag="button" replace>内容</router-link>
+    <router-link :to="{path:'/profile',query:{name:'wukong',age:'47',height:'12'}}" tag="button" replace>档案</router-link>
+    <router-link :to="'/user/'+id" tag="button" replace>用户</router-link>
+  
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+ data(){
+   return{
+     id:'123'
+   }
+ }
+  
+ 
+}
+</script>
+
+<style>
+#app {
+}
+</style>
