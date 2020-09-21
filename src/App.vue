453de5b5caaa96f1e28dcc670c29930762383c63
angten <2258965667@qq.com>
2020-09-21 19:34:55 +0800

    参数传递与前置或后置守卫或钩子

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <router-link to="/home" tag="button" replace>主页</router-link>
     <router-link to="/about" tag="button" replace>内容</router-link>
+    <router-link :to="{path:'/profile',query:{name:'wukong',age:'47',height:'12'}}" tag="button" replace>档案</router-link>
+    <router-link :to="'/user/'+id" tag="button" replace>用户</router-link>
   
     <router-view></router-view>
   </div>
@@ -9,7 +11,14 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+ data(){
+   return{
+     id:'123'
+   }
+ }
+  
+ 
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -5,25 +5,38 @@ import Router from 'vue-router'
  const about=() =>import('../components/about')
  const homelove=() =>import('../components/homelove')
  const homemessage=() =>import('../components/homemessage')
+ const profile=()=>import('../components/profile')
+ const user=()=>import('../components/user')
 
 Vue.use(Router)
 
 const routes=[
   {
-    path:'/home',
-    component:home,
-    children:[
-      {
-        path:'',
-        redirect:'love'
+        path:'/',
+        redirect:'/home'
       },
       {
+    path:'/home',
+    component:home,
+    meta:{
+      title:"首页"
+    },
+    children:[
+      
+     
+      {
         path:'love',
+        meta:{
+          title:"首页-love"
+        },
         component:homelove
       },
 
       {
        path:'message',
+       meta:{
+        title:"首页-message"
+      },
        component:homemessage
       }
     ]
@@ -31,16 +44,47 @@ const routes=[
   },
   {
     path:'/about',
+    meta:{
+      title:"关于"
+    },
     component:about
+  },
+  {
+    path:'/profile',
+    meta:{
+      title:"档案"
+    },
+    component:profile
+
+  },
+  {
+    path:'/user/:id',
+    meta:{
+      title:"用户"
+    },
+    component:user
   }
 
 ]
 
 
-
-
-
-export default new Router({
+const router=new Router({
   routes,
   mode:"history"
 })
+//  前置守卫（guard）   前置钩子(hook)
+router.beforeEach((to,from,next)=>{
+  document.title=to.meta.title
+    //  document.title=to.matched[0].meta.title  //关于 children组件的 网页title的 显示  是 父组件还是 children组件自己的
+  console.log(to)
+  next();
+})
+//后置守卫      后置钩子
+router.afterEach((to,from)=>{
+
+})
+
+
+
+ 
+export default router
